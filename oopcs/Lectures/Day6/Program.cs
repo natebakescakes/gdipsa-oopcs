c657b59fde35f5b7d27568cb4ce9c2bc5386e605
natebakescakes <natebakescakes@gmail.com>
2018-02-22 16:12:08 +0800

    Add Day 6 Lecture

---
+++ oopcs/Lectures/Day6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lectures.Day6
+{
+    class StringLengthComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return x.Length.CompareTo(y.Length);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] wordList = { "able", "ale", "apple", "bale", "kangaroo" };
+            //Array.Sort(wordList, new StringLengthComparer());
+            //Array.Reverse(wordList);
+
+            //Console.WriteLine(String.Join(", ", wordList));
+
+            //Customer a, b;
+            //a = new Customer();
+            //b = new Customer();
+
+            //Console.WriteLine(a == b);
+            //Console.WriteLine(a.Equals(b));
+        }
+    }
+
+    //class Customer
+    //{
+    //    private string name;
+    //}
+}
